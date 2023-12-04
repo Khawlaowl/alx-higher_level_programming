4b2be146a89234b8df4040e7815b84ebf674792e
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-12-05 00:29:22 +0100

    o

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-'''1-my_list.py
-'''
+"""
+subclass from List base class
+"""
 
 
 class MyList(list):
-    ''' This class inherits from list() '''
+    """a subclass of list base class"""
+    def __init__(self):
+        """object constructor"""
+        super().__init__()
+        """Access the methods of the Base class"""
 
     def print_sorted(self):
-        """ prints the sorted list """
+        """print_sorted method"""
         print(sorted(self))
