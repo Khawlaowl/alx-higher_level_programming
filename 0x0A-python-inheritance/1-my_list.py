f2b671958bcc09b95b17091077c4a50578d780d7
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-12-05 00:40:05 +0100

    o

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+This module has a class MyList that inherits from
+list. Has a public instance method print_sorted that
+prints the list in ascending order
+
+"""
+
+
 class MyList(list):
-    """A subclass of the list base class"""
+    """MyList class subclass of list
+    Attributes:
+    print_sorted: prints the list in a sorted fashion
+    """
 
     def print_sorted(self):
-        """Print the list sorted in ascending order"""
-        sorted_list = sorted(self)
-        print(sorted_list)
-        return sorted_list  # If you want to return the sorted list
+        """print_sorted method prints list sorted"""
+        print(sorted(self))
