98a262a7da320b471f6157d6bd6f3b9c942411d9
Khawlaowl <khawlahilli20045@gmail.com>
2023-12-05 21:57:01 +0000

    f

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+d_file module.
+
+Contains a function that reads a text file.
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r') as f:
+        print(f.read(), end='')
