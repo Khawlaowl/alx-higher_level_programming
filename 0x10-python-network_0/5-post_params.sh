5b4ff84de00d6e24c634802b89e21621bef65907
Khawlaowl <khawlahilli20045@gmail.com>
2024-02-29 14:20:44 -0800

    f

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request
-curl -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+# Script that sends a POST request and displays the body response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
